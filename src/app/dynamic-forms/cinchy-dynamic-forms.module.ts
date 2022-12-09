cba569c898bc511f6172d15d9a42200f361aab00
Dominic Aquilina <aquilina.dominic@gmail.com>
2022-12-09 10:07:42 -0500

    Reverting some cleanup changes

--- src/app/dynamic-forms/cinchy-dynamic-forms.module.ts
+++ src/app/dynamic-forms/cinchy-dynamic-forms.module.ts
@@ -1,33 +1,33 @@
 import { MultiChoiceDirective } from './fields/cinchy-form-multichoice.directive';
 import { ChoiceDirective } from './fields/cinchy-form-choice.directive';
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatTableModule } from "@angular/material/table";
-import { MatInputModule } from "@angular/material/input";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatNativeDateModule } from "@angular/material/core";
-import { MatIconModule } from "@angular/material/icon";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatSelectModule } from "@angular/material/select";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { FilterPipeModule } from "ngx-filter-pipe";
-import { CinchyModule } from "@cinchy-co/angular-sdk";
-import { NgxSelectModule, INgxSelectOptions } from "ngx-select-ex";
-import { NgxSpinnerModule } from "ngx-spinner";
-import { MatTabsModule } from "@angular/material/tabs";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { ToastrModule } from "ngx-toastr";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { DigitOnlyModule } from "@uiowa/digit-only";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FilterPipeModule } from 'ngx-filter-pipe';
+import { CinchyModule } from '@cinchy-co/angular-sdk';
+import { NgxSelectModule, INgxSelectOptions } from 'ngx-select-ex';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { ToastrModule } from 'ngx-toastr';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DigitOnlyModule } from '@uiowa/digit-only';
 
 //#region Custom
 import { AddRichTextLinkDialogComponent } from "./dialogs/add-rich-text-link/add-rich-text-link.component";
@@ -54,28 +54,28 @@ import { MessageDialogComponent } from './message-dialog/message-dialog.componen
 import { SharedModule } from '../shared/shared.module';
 import { LinkMultichoice } from './fields/cinchy-form-link-multichoice';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
-import "brace";
-import "brace/index";
-import "brace/ext/language_tools";
-import "brace/snippets/text";
-import "brace/snippets/sql";
-import "brace/snippets/html";
-import "brace/snippets/sqlserver";
-import "brace/snippets/javascript";
-import "brace/snippets/xml";
-import "brace/snippets/json";
-import { AceEditorModule } from "ng2-ace-editor";
-import "ace-builds/webpack-resolver";
-import { DatePipe } from "@angular/common";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import 'brace';
+import 'brace/index';
+import 'brace/ext/language_tools';
+import 'brace/snippets/text';
+import 'brace/snippets/sql';
+import 'brace/snippets/html';
+import 'brace/snippets/sqlserver';
+import 'brace/snippets/javascript';
+import 'brace/snippets/xml';
+import 'brace/snippets/json';
+import { AceEditorModule } from 'ng2-ace-editor';
+import 'ace-builds/webpack-resolver';
+import { DatePipe } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-window["ace"]["require"] = window["ace"]["acequire"];
+window['ace']['require'] = window['ace']['acequire'];
 //#endregion
 
 //#region Select Dropdownlist options
 const CustomSelectOptions: INgxSelectOptions = { // Check the interface for more options
-  optionValueField : "id",
-  optionTextField  : "label",
+  optionValueField : 'id',
+  optionTextField  : 'label',
   keepSelectedItems: true
 };
 //#endregion
