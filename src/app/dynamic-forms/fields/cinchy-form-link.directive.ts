2547fb3de18f03f72e4858ff06837e311bd14962
DAquilinaCinchy <dominic.aquilina@cinchy.com>
2023-03-25 20:38:54 -0400

    Fixing typo

--- src/app/dynamic-forms/fields/cinchy-form-link.directive.ts
+++ src/app/dynamic-forms/fields/cinchy-form-link.directive.ts
@@ -317,7 +317,7 @@ export class LinkDirective implements OnInit {
       let tableColumnQuery: string = "select tc.[Table].[Domain].[Name] as \"Domain\", tc.[Table].[Name] as \"Table\", tc.[Name] as \"Column\" from [Cinchy].[Cinchy].[Table Columns] tc where tc.[Deleted] is null and tc.[Table].[Deleted] is null and tc.[Cinchy Id] = " + linkTargetId;
       this.metadataQueryResult = (await this._cinchyService.executeCsql(tableColumnQuery, null).toPromise()).queryResult.toObjectArray();
 
-      const formFieldsJsonData = JSON.parse(this.field.cinchyColumn.formFieldsJsonData);
+      const formFieldsJsonData = JSON.parse(this.field.cinchyColumn.FormFieldsJsonData);
       if (formFieldsJsonData?.Columns) {
         currentFieldJson = formFieldsJsonData.Columns.find(field => field.name === this.field.cinchyColumn.name);
       }
