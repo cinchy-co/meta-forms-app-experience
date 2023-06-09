97d53c7ecc2b55aa60addc6a934222382fb531f8
Alvin Ho <alvin.ho@cinchy.co>
2023-06-09 00:39:07 -0400

    Fix link fields using display columns aren't displaying correctly after selecting from dropdown

--- src/app/dynamic-forms/service/cinchy-dropdown-dataset/cinchy-dropdown-options.ts
+++ src/app/dynamic-forms/service/cinchy-dropdown-dataset/cinchy-dropdown-options.ts
@@ -3,7 +3,7 @@ export class DropdownOption {
   constructor(
     public id: string,
     public label: string,
-    public displayOnlyLabel?: any,
+    public displayOnlyLabel?: string,
     public group?: string
   ) {}
 }
