038195fc614b16fd5485b04d898e710906f98ab2
Bibin Babu <bibin.babu@cinchy.com>
2023-08-14 13:53:47 +0530

    Corrected rxjs by removing Subject since it was failing in the pipeline.

--- src/app/services/section-service.ts
+++ src/app/services/section-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 import { SpinnerCondition } from '../dynamic-forms/models/cinchy-spinner.model';
 
 @Injectable()
