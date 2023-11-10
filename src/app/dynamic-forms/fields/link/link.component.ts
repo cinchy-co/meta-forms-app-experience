a1d1482effbeaef6c7d4096a43ae20b8ecaf1088
Dominic Aquilina <119648879+DAquilinaCinchy@users.noreply.github.com>
2023-11-10 17:14:47 -0500

    CIN-06632: Defect resolution (#183)

--- src/app/dynamic-forms/fields/link/link.component.ts
+++ src/app/dynamic-forms/fields/link/link.component.ts
@@ -14,6 +14,7 @@ import {
   ViewChild
 } from "@angular/core";
 import { coerceBooleanProperty } from "@angular/cdk/coercion";
+import { MatSelectChange } from "@angular/material/select";
 
 import { CinchyService } from "@cinchy-co/angular-sdk";
 
@@ -479,12 +480,16 @@ export class LinkComponent implements OnChanges, OnInit {
   /**
    * Resolves the selectedValue when the user selects an option from the autocomplete
    */
-  onOptionSelected(option: DropdownOption): void {
+  onOptionSelected(event: MatSelectChange, option: DropdownOption): void {
 
-    // We don't need to explicitly set autocompleteText because the value of the selected option already does that
-    this.selectedValue = option;
+    // This function will also be called on the previously-selected value, if any, so we're just
+    // checking to see if the given option is the one we care about
+    if (event.source.selected) {
+      // We don't need to explicitly set autocompleteText because the value of the selected option already does that
+      this.selectedValue = option;
 
-    this.valueChanged();
+      this.valueChanged();
+    }
   }
 
 
